d992678cc24cd1a6424fd59ac40bfb36b6364bad
Gustavo Lima <gustavomdcl@gmail.com>
2020-07-13 17:13:01 -0300

    CRUD Backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects,setProjects] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response=>{
+      setProjects(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: `Projeto ${Date.now()}`,
+      url: 'https://github.com/Rocketseat/',
+      techs: ['ReactJS','NodeJS'],
+    });
+
+    const project = response.data;
+
+    setProjects([...projects,project]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository({id,index}) {
+    await api.delete(`repositories/${id}`);
+
+    const repositories = [...projects];
+    repositories.splice(index,1)
+
+    setProjects(repositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {projects.map((project,index)=><li key={project.id}>
+          {project.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository({id:project.id,index})}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
